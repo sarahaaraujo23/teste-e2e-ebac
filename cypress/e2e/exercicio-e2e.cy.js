0902bf76ff32549c7d09fe3f7f1954e83e27bd70
Sarah Araujo <sarahjanne2008@gmail.com>
2024-09-12 16:47:03 -0300

    Atividade EBAC

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-
+import produtosPage from "../support/page_objects/produtos.page";
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
       Quero acessar a Loja EBAC 
@@ -10,11 +10,33 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+      cy.visit('/produtos')
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
       //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
+      cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProdutoLista(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(dados[0].tamanho, dados[0].cor,dados[0].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+        cy.get('.woocommerce-message > .button').click()
+        cy.get('.checkout-button').click()
+        cy.get('.showlogin').click()
+        cy.get('#username').type('sarahjanne2008@gmail.com')
+        cy.get('#password').type('ebac123')
+        cy.get('.woocommerce-button').click()
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+        cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+
+
+
+    })
+      
+      
+    
+      
+
       
   });
 
---
+++ cypress/support/page_objects/produtos.page.js
@@ -0,0 +1,17 @@
+class ProdutosPage {
+
+    buscarProdutoLista(nomeProduto) {
+        cy.get('.products > .row')
+        .contains(nomeProduto)
+        .click()
+
+    }
+    addProdutoCarrinho (tamanho, cor, quantidade) {
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get('.button-variable-item-' + cor).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+    }
+}
+
+export default new ProdutosPage()
